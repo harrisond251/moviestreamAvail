1dd317315fa501ef6a912e19e5c3adcee51dbc01
LonelyMitoc <jamesmatsu@gmail.com>
2022-09-26 20:54:38 -0700

    frontpage script with timeout error message

---
+++ assets/js/frontpage.js
@@ -0,0 +1,26 @@
+var searchInputEl = document.querySelector('#title');
+var errorMessage = document.querySelector('#error-message');
+
+function handleSearchFormSubmit(event) {
+    event.preventDefault();
+
+    var searchInputVal = document.querySelector('#title').value;
+    var yearInputVal = document.querySelector('#year').value;
+
+    if (!searchInputVal) {
+        errorMessage.textContent = 'Please enter a search input value';
+
+        setTimeout(function(){
+            errorMessage.textContent = '';
+        }, 2000);
+
+        return;
+    }
+
+    var queryString = './searchpage.html?q=' + searchInputEl + '&y=' + yearInputVal;
+
+    location.assign(queryString);
+}
+
+var searchBtn = document.querySelector('#search-btn');
+searchBtn.addEventListener('click', handleSearchFormSubmit);
