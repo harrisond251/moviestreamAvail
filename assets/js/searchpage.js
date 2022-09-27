c6626f33bbaecd06b65203a8bfe2a9b96af63edb
ncortes04 <nickapoleon@gmail.com>
2022-09-26 21:11:47 -0700

    added api to searchpage javascript

--- assets/js/searchpage.js
+++ assets/js/searchpage.js
@@ -17,3 +17,39 @@ fetch(moviePosterAPIURL)
     moviePosterEl.setAttribute('src', rootPosterURL + moviePosterURL);
   })
 
+  var apiUrl = "https://streaming-availability.p.rapidapi.com/get/basic?country=us"
+  var hell = ''
+  const option2 = {
+    method: 'GET',
+  
+  };
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': 'b564bfa71cmshde756ebe5dfec26p17269bjsnb2462c928d37',
+      'X-RapidAPI-Host': 'streaming-availability.p.rapidapi.com'
+    }
+  };
+  
+  function fetchMovieData(serviceId) { 
+    fetch( apiUrl + "&imdb_id=" + serviceId + '&output_language=en', options)
+      .then(response => response.json())
+      .then(response => console.log(response))
+      .catch(err => console.error(err));
+  };
+  
+  var api2Url = "http://www.omdbapi.com/?apikey=c2500aea"
+  
+  fetch( api2Url + "&t=jumanji" , option2)
+    .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+        var imdb_id = data.imdbID;
+      console.log(imdb_id)
+      fetchMovieData(imdb_id)
+      }) 
+  
+      
+  
