a6748ea6293e8eefe3afdccd58a7fd751604615e
LonelyMitoc <jamesmatsu@gmail.com>
2022-09-26 21:31:31 -0700

    some css changes

--- assets/js/searchpage.js
+++ assets/js/searchpage.js
@@ -1,7 +1,7 @@
 var movieName = 'step brothers';
 
 // Defining variables related to the movie poster API
-var moviePosterEl = document.querySelector('#imgpic');
+var moviePosterEl = document.querySelector('#movie-poster');
 var moviePosterAPIKey = '15d2ea6d0dc1d476efbca3eba2b9bbfb&query';
 var moviePosterAPIURL = 'https://api.themoviedb.org/3/search/movie?api_key=' + moviePosterAPIKey + '&query=' + movieName;
 var rootPosterURL = 'http://image.tmdb.org/t/p/w500/';
