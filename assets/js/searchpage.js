f153c6c16bcf5b1d69124146c0ca19f3beb92361
ncortes04 <nickapoleon@gmail.com>
2022-09-26 21:31:54 -0700

    update java and HTML

--- assets/js/searchpage.js
+++ assets/js/searchpage.js
@@ -16,9 +16,9 @@ fetch(moviePosterAPIURL)
     var moviePosterURL = data.results[0].poster_path;
     moviePosterEl.setAttribute('src', rootPosterURL + moviePosterURL);
   })
-
+/* api url for streaming availability*/
   var apiUrl = "https://streaming-availability.p.rapidapi.com/get/basic?country=us"
-  var hell = ''
+/* defines method for fetch methods */
   const option2 = {
     method: 'GET',
   
@@ -30,23 +30,35 @@ fetch(moviePosterAPIURL)
       'X-RapidAPI-Host': 'streaming-availability.p.rapidapi.com'
     }
   };
-  
+  /* function that stores the data that displays results for streaming availability */
   function fetchMovieData(serviceId) { 
     fetch( apiUrl + "&imdb_id=" + serviceId + '&output_language=en', options)
       .then(response => response.json())
       .then(response => console.log(response))
       .catch(err => console.error(err));
   };
-  
+  /* api that stores url for omdb api */
   var api2Url = "http://www.omdbapi.com/?apikey=c2500aea"
-  
+  /* fetch for omdb api */
   fetch( api2Url + "&t=jumanji" , option2)
     .then(function (response) {
+      /* converts response to json */
         return response.json();
       })
       .then(function (data) {
+        /* creating varibles for results and sending them to html */
         console.log(data);
         var imdb_id = data.imdbID;
+        var movietitle = data.Title;
+        var plot = data.Plot
+        var director = data.Director
+        var actors = data.Actors
+
+        document.getElementById('movieTitle').innerHTML = movietitle
+        document.getElementById('plotText').innerHTML = plot
+        document.getElementById('directorText').innerHTML = director
+        document.getElementById('actorsText').innerHTML = actors
+
       console.log(imdb_id)
       fetchMovieData(imdb_id)
       }) 
